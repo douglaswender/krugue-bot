59b3c66b03da1b4a903c711d3b435d9a43c9ba38
Douglas Wender <dglswender@gmail.com>
2021-04-15 16:38:44 -0400

    make it better

--- commands/utility/patch.js
+++ commands/utility/patch.js
@@ -2,10 +2,14 @@ const repository = require('../../utils/repository');
 
 module.exports = {
     name: 'patch',
-	description: 'Patch notes!',
-	async execute(message) {
-        repository.getLastPatchVersion().then(response=>{
-            if(response !== null) return message.channel.send(`Estamos no patch ${response}`);
+    description: 'Patch notes!',
+    async execute(message) {
+        repository.getLastPatchVersion().then(response => {
+            if (response !== null) {
+                message.channel.send(`Estamos no patch ${response}`);
+                message.channel.send(`LOL: https://br.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-${patchVersion.replace('.', '-')}/`);
+				message.channel.send(`TFT: https://br.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-${patchVersion.replace('.', '-')}-do-teamfight-tactics/`);
+            }
         });
-	},
+    },
 }
